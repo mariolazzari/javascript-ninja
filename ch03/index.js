afd08caebfa9ecbfaea6b47c95096026cbdf7060
Mario Lazzari <mario.lazzari@gmail.com>
2019-06-24 18:01:51 +0200

    Done lesson 2 chapter 3

---
+++ ch03/index.js
@@ -0,0 +1,10 @@
+// for
+for (let i = 0; i < 10; i++) {
+  console.log("i", i);
+}
+
+const names = ["Mario", "Mariarosa", "maria"];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
