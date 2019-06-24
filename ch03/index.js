6b0ab0548c0c3d655347f7e7992da07ef20a3ca8
Mario Lazzari <mario.lazzari@gmail.com>
2019-06-24 19:43:01 +0200

    Done lesson 8 chapter 2

--- ch03/index.js
+++ ch03/index.js
@@ -1,10 +1,59 @@
 // for
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 5; i++) {
   console.log("i", i);
 }
 
-const names = ["Mario", "Mariarosa", "maria"];
-
+const names = ["Mario", "Mariarosa", "Maria"];
 for (let i = 0; i < names.length; i++) {
   console.log(names[i]);
 }
+
+// while
+let i = 0;
+while (i < 5) {
+  console.log("i", i);
+  i++;
+}
+
+i = 0;
+while (i < names.length) {
+  console.log(names[i]);
+  i++;
+}
+
+// do-while
+i = 5;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
+
+// if
+const age = 20;
+if (age >= 20) {
+  console.log(`You are ${age} years old or more`);
+}
+if (names.length > 2) {
+  console.log("More than 2 names");
+}
+
+// logical operator
+const password = "p@ss";
+if (password >= 12 && password.includes("@")) {
+  console.log("Strong password");
+} else if (
+  password.length >= 8 ||
+  (password.includes("@") && password.length > 5)
+) {
+  console.log("Good password");
+} else {
+  console.log("Bad password");
+}
+
+// not
+let user = false;
+if (!user) {
+  console.log("user logged.");
+}
+
+// breack and continue
